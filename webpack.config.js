bb00bc2016765940e7ab5d1191c162fe1cb5d8f0
Eric Fuller <info@glassmoonmedia.com>
2017-02-17 16:21:52 -0500

    Cleanup our Webpack config files because ESLint :grin:

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef, one-var */
+
 /**
  * Webpack Development Config
  *
@@ -7,58 +9,67 @@
 /**
  * System imports.
  */
-const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const autoprefixer = require('autoprefixer');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const path = require( 'path' );
+const webpack = require( 'webpack' );
+const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
+const autoprefixer = require( 'autoprefixer' );
+const BrowserSyncPlugin = require( 'browser-sync-webpack-plugin' );
 
+//noinspection JSUnresolvedFunction
 /**
  * Webpack config object.
  */
 const config = {
+
 	// The entry point of our app. Where our dependency graph starts.
 	entry: [
 		'./src/index'
 	],
+
 	// The output location for our compiled bundle.
 	output: {
 		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'assets/js')
+		path: path.resolve( __dirname, 'assets/js' )
 	},
+
 	// Loaders. These transform our code by running them through transpilers.
 	module: {
 		rules: [
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
-				loader: "babel-loader",
+				loader: 'babel-loader'
 			},
 			{
 				test: /\.scss$/,
 				loader: ExtractTextPlugin.extract({
 					fallbackLoader: 'style-loader',
-					loader: "css-loader!postcss-loader!sass-loader",
-				}),
+					loader: 'css-loader!postcss-loader!sass-loader'
+				})
 			}
 		]
 	},
+
 	// Here we are exposing a jQuery global that can be used in modules since WordPress is using it.
 	externals: {
 		'jQuery': 'jQuery'
 	},
+
 	// Webpack plugins - These are like addons that can provide additional functionality.
 	plugins: [
+
 		// Extract the CSS file.
-		new ExtractTextPlugin('../css/wp-todo.css'),
+		new ExtractTextPlugin( '../css/wp-todo.css' ),
+
 		// Configure autoprefixer.
 		new webpack.LoaderOptionsPlugin({
 			options: {
 				postcss: [
-					autoprefixer(),
+					autoprefixer()
 				]
 			}
 		}),
+
 		// Browsersync is a better option when working in a WordPress environment.
 		new BrowserSyncPlugin({
 			open: false,
@@ -66,8 +77,9 @@ const config = {
 			host: 'localhost',
 			port: '3000',
 			proxy: 'learnwp.dev'
-		}),
+		})
 	],
+
 	// This allows for JS and SCSS source maps.
 	devtool: 'inline-source-map'
 };
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef, one-var */
+
 /**
  * Webpack Production Config
  *
@@ -7,58 +9,66 @@
 /**
  * System imports.
  */
-const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const autoprefixer = require('autoprefixer');
+const path = require( 'path' );
+const webpack = require( 'webpack' );
+const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
+const autoprefixer = require( 'autoprefixer' );
 
+//noinspection JSUnresolvedFunction
 /**
  * Webpack config object.
  */
 const prodConfig = {
+
 	// The entry point of our app. Where our dependency graph starts.
 	entry: [
 		'./src/index'
 	],
+
 	// The output location for our compiled bundle.
 	output: {
 		filename: 'bundle.min.js',
-		path: path.resolve(__dirname, 'assets/js')
+		path: path.resolve( __dirname, 'assets/js' )
 	},
+
 	// Loaders. These transform our code by running them through transpilers.
 	module: {
 		rules: [
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
-				loader: "babel-loader",
+				loader: 'babel-loader'
 			},
 			{
 				test: /\.scss$/,
 				exclude: /node_modules/,
 				loader: ExtractTextPlugin.extract({
 					fallbackLoader: 'style-loader',
-					loader: "css-loader!postcss-loader!sass-loader",
-				}),
+					loader: 'css-loader!postcss-loader!sass-loader'
+				})
 			}
 		]
 	},
+
 	// Here we are exposing a jQuery global that can be used in modules since WordPress is using it.
 	externals: {
 		'jQuery': 'jQuery'
 	},
 	plugins: [
+
 		// Extract the CSS file.
-		new ExtractTextPlugin('../css/wp-todo.css'),
+		new ExtractTextPlugin( '../css/wp-todo.css' ),
+
 		// Configure autoprefixer.
 		new webpack.LoaderOptionsPlugin({
 			options: {
 				postcss: [
-					autoprefixer(),
+					autoprefixer()
 				]
 			}
 		})
 	],
+
 	// This allows for JS and SCSS source maps.
 	devtool: 'cheap-source-map'
 };
