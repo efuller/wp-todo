3b1a33423bd556b9787ed940c5db701b1c07ebd2
Eric Fuller <info@glassmoonmedia.com>
2017-03-24 11:06:43 -0400

    More API changes as well as the Unit Tests that go along with them. Working to be a bit more explicit in the API functions provided.

--- src/api/API.js
+++ src/api/API.js
@@ -1,136 +1,163 @@
 import delay from './delay';
 import uuid from 'uuid';
 
-const data = [
-	{
-		id: 1,
-		name: 'My First List',
-		active: true,
-		deleted: false,
-		showCompleted: false,
-		showDeleted: false,
-		todos: [
-			{
-				id: 1,
-				title: 'Here is todo number 1',
-				description: 'Here is the description for number 1',
-				deleted: false,
-				completed: true
-			},
-			{
-				id: 2,
-				title: 'Here is todo number 2',
-				description: 'Here is the description for number 1',
-				deleted: false,
-				completed: true
-			},
-			{
-				id: 3,
-				title: 'Here is todo number 2',
-				description: 'Here is the description for number 1',
-				deleted: false,
-				completed: true
-			}
-		]
-	},
-	{
-		id: 2,
-		name: 'My Second List',
-		active: false,
-		deleted: true,
-		showCompleted: false,
-		showDeleted: false,
-		todos: [
-			{
-				id: 5,
-				title: 'List 2 - Todo 1',
-				description: 'Here is the description for number 1',
-				deleted: false,
-				completed: false
-			},
-			{
-				id: 6,
-				title: 'List 2 - Todo 2',
-				description: 'Here is the description for number 1',
-				deleted: true,
-				completed: true
-			},
-			{
-				id: 7,
-				title: 'List 2 - Todo 3',
-				description: 'Here is the description for number 1',
-				deleted: false,
-				completed: true
-			}
-		]
-	},
-	{
-		id: 3,
-		name: 'My Third List',
-		active: false,
-		deleted: false,
-		showCompleted: false,
-		showDeleted: false,
-		todos: [
-			{
-				id: 10,
-				title: 'List 3 - Todo 1',
-				description: 'Here is the description for number 1',
-				deleted: false,
-				completed: false
-			},
-			{
-				id: 11,
-				title: 'List 3 - Todo 2',
-				description: 'Here is the description for number 1',
-				deleted: true,
-				completed: true
-			},
-			{
-				id: 12,
-				title: 'List 3 - Todo 3',
-				description: 'Here is the description for number 1',
-				deleted: false,
-				completed: true
-			},
-			{
-				id: 13,
-				title: 'List 3 - Todo 4',
-				description: 'Here is the description for number 1',
-				deleted: true,
-				completed: true
-			},
-			{
-				id: 14,
-				title: 'List 3 - Todo 5',
-				description: 'Here is the description for number 1',
-				deleted: false,
-				completed: false
-			}
-		]
-	}
-];
+const data = {
+	primaryList: 1,
+	lists: [
+		{
+			id: 1,
+			name: 'My First List',
+			active: true,
+			deleted: false,
+			showCompleted: false,
+			showDeleted: false,
+			todos: [
+				{
+					id: 1,
+					title: 'Here is todo number 1',
+					description: 'Here is the description for number 1',
+					deleted: false,
+					completed: true
+				},
+				{
+					id: 2,
+					title: 'Here is todo number 2',
+					description: 'Here is the description for number 1',
+					deleted: false,
+					completed: true
+				},
+				{
+					id: 3,
+					title: 'Here is todo number 2',
+					description: 'Here is the description for number 1',
+					deleted: false,
+					completed: true
+				}
+			]
+		},
+		{
+			id: 2,
+			name: 'My Second List',
+			primary: false,
+			active: false,
+			deleted: true,
+			showCompleted: false,
+			showDeleted: false,
+			todos: [
+				{
+					id: 5,
+					title: 'List 2 - Todo 1',
+					description: 'Here is the description for number 1',
+					deleted: false,
+					completed: false
+				},
+				{
+					id: 6,
+					title: 'List 2 - Todo 2',
+					description: 'Here is the description for number 1',
+					deleted: true,
+					completed: true
+				},
+				{
+					id: 7,
+					title: 'List 2 - Todo 3',
+					description: 'Here is the description for number 1',
+					deleted: false,
+					completed: true
+				}
+			]
+		},
+		{
+			id: 3,
+			name: 'My Third List',
+			primary: false,
+			active: false,
+			deleted: false,
+			showCompleted: false,
+			showDeleted: false,
+			todos: [
+				{
+					id: 10,
+					title: 'List 3 - Todo 1',
+					description: 'Here is the description for number 1',
+					deleted: false,
+					completed: false
+				},
+				{
+					id: 11,
+					title: 'List 3 - Todo 2',
+					description: 'Here is the description for number 1',
+					deleted: true,
+					completed: true
+				},
+				{
+					id: 12,
+					title: 'List 3 - Todo 3',
+					description: 'Here is the description for number 1',
+					deleted: false,
+					completed: true
+				},
+				{
+					id: 13,
+					title: 'List 3 - Todo 4',
+					description: 'Here is the description for number 1',
+					deleted: true,
+					completed: true
+				},
+				{
+					id: 14,
+					title: 'List 3 - Todo 5',
+					description: 'Here is the description for number 1',
+					deleted: false,
+					completed: false
+				}
+			]
+		}
+	]
+};
 
 /**
  * Fake API Class
  */
 class API {
-	/**
-	 * Get lists.
-	 * By default, get all lists not marked as deleted.
-	 * @param   {Boolean} all     If true, return all lists.
-	 * @param   {Boolean} deleted If true, get lists marked as deleted.
-	 * @returns {Promise} lists   The list items.
-	 */
-	static getLists( all = false, deleted = false ) {
+
+	static getPrimaryList() {
 		return new Promise( ( resolve ) => {
 			setTimeout( () => {
-				if ( all ) {
-					resolve( data );
-				}
-				const lists = data.filter( ( list ) => deleted === list.deleted );
-				resolve( lists );
+				const primaryListId = data.primaryList;
+
+				API.getListById( primaryListId )
+					.then( ( list ) => {
+						resolve( list );
+					});
 			}, delay );
+		});
+	}
+
+	/**
+	 * Get lists.
+	 * By default, get all lists.
+	 * @returns {Promise} lists   The list items.
+	 */
+	static getLists() {
+		return new Promise( ( resolve ) => {
+			setTimeout( () => {
+				resolve( data.lists );
+			}, delay );
+		});
+	}
+
+	/**
+	 * Get lists marked as deleted.
+	 * @returns {Promise} Lists marked as deleted.
+	 */
+	static getDeletedLists() {
+		return new Promise( ( resolve ) => {
+			API.getLists()
+				.then( ( lists ) => {
+					const deleted = lists.filter( ( list ) => true === list.deleted );
+					resolve( deleted );
+				});
 		});
 	}
 
@@ -142,15 +169,15 @@ class API {
 	static getListById( listId ) {
 		return new Promise( ( resolve ) => {
 			setTimeout( () => {
-				const list = data.filter( ( list ) => list.id === listId );
-				resolve( list );
+				const list = data.lists.filter( ( list ) => list.id === listId );
+				resolve( list[0]);
 			}, delay );
 		});
 	}
 
 	/**
 	 * Delete a list.
-	 * @param   {Boolean} listId the id of the list.
+	 * @param   {Number}  listId the id of the list.
 	 * @returns {Promise} The deleted list.
 	 */
 	static deleteList( listId ) {
@@ -158,11 +185,11 @@ class API {
 			setTimeout( () => {
 				API.getListById( listId )
 					.then( ( list ) => {
-						const listIndex = data.findIndex( list => {
+						const listIndex = data.lists.findIndex( list => {
 							return list.id === listId;
 						});
-						data.splice( listIndex, 1 );
-						resolve( list[0]);
+						data.lists.splice( listIndex, 1 );
+						resolve( list);
 					});
 			}, delay );
 		});
@@ -171,16 +198,15 @@ class API {
 	/**
 	 * Get todos from a list. By default, get todos not marked as deleted.
 	 * @param   {Number}  listId  The id of the list to retrieve todos from.
-	 * @param   {Boolean} deleted If true, this will return todos marked as deleted.
 	 * @returns {Promise} todos   The set of matching todos.
 	 */
-	static getTodos( listId, deleted = false ) {
+	static getTodos( listId ) {
 		return new Promise( ( resolve ) => {
 			setTimeout( () => {
 				API.getListById( listId )
 					.then( ( list ) => {
-							const todos = list[0].todos;
-							resolve( todos.filter( todos => deleted === todos.deleted ) );
+							const todos = list.todos;
+							resolve( todos );
 						});
 					});
 			}, delay );
@@ -198,6 +224,7 @@ class API {
 			setTimeout( () => {
 				API.getTodos( listId )
 					.then( ( todos ) => { // @todo This is not immutable.
+						debugger;
 						const todoIndex = todos.findIndex( todo => {
 							return todo.id === todoId;
 						});
--- test/API.test.js
+++ test/API.test.js
@@ -3,71 +3,65 @@ import API from '../src/api/API';
 
 test( 'The API', ( t ) => {
 
-		API.getLists()
-			.then( ( lists ) => {
-				const expected = 2;
-				const actual = lists.length;
-				t.equal( actual, expected, 'API.getLists(): should return 3 lists.' );
-			});
-		API.getLists( false, true )
-			.then( ( lists ) => {
-				const expected = 1;
-				const actual = lists.length;
-				t.equal( actual, expected, 'API.getLists(true): should return 1 deleted list.' );
-			});
-		API.getListById( 2 )
-			.then( ( list ) => {
-				const expected = 'My Second List';
-				const actual = list[0].name;
-				t.equal( actual, expected, 'API.getListByID(): should return list 2 named My Second List' );
-			});
-		API.getTodos( 2 )
-			.then( ( todos ) => {
-				const expected = 2;
-				const actual = todos.length;
-				t.equal( actual, expected, 'API.getTodo(): should get todos from list 5. There should be 5 todos.' );
-			});
-		API.updateTodo( 3, 12, { title: 'List 3 - Todo 3, Updated' })
-			.then( ( todo ) => {
-				const expected = 'List 3 - Todo 3, Updated';
-				const expectedId = 12;
-				const actual = todo.title;
-				const actualId = todo.id;
-				t.equal( actual, expected, 'API.updateTodo(): should update title to List 3 - Todo 3, Updated' );
-				t.equal( actualId, expectedId, 'API.updateTodo(): should update todo with id of 12.' );
-			});
-		API.deleteTodo( 2, 7 )
-			.then( ( todos ) => {
-				const expectedNumberOfTodos = 1;
-				const actualNumberOfTodos = todos.length;
-				t.equal( actualNumberOfTodos, expectedNumberOfTodos, 'API.deleteTodo(): should return 1 todos.' );
-			});
-		API.deleteTodo( 1, 3 )
-			.then( ( todos ) => {
-				const expectedNumberOfTodos = 2;
-				const actualNumberOfTodos = todos.length;
-				t.equal( actualNumberOfTodos, expectedNumberOfTodos, 'API.deleteTodo(): should return 1 todos.' );
-			});
-		API.completeTodo( 3, 14 )
-			.then( ( todo ) => {
-				const expectedTodoId = 14;
-				const expectedCompleted = true;
-				const actualTodosId = todo.id;
-				const actualCompleted = todo.completed;
-				t.equal( actualTodosId, expectedTodoId, 'API.completeTodo(): should update todo with id 14.' );
-				t.equal( actualCompleted, expectedCompleted, 'API.completeTodo(): should be completed' );
-			});
-		API.deleteList( 2 )
-			.then( ( list ) => {
-				const expectNumberOfLists = 2;
-				const expectDeletedListId = 2;
-				const actualDeletedListId = list.id;
-				API.getLists( true )
-					.then( ( lists ) => {
-						const actualNumberOfLists = lists.length;
-						t.equal( actualDeletedListId, expectDeletedListId, 'API.deleteList(2): should delete the list with Id 2.' );
-						t.equal( actualNumberOfLists, expectNumberOfLists, 'API.deleteList(2): should result with there being two lists.' );
-					});
-			});
+	API.getLists()
+		.then( ( lists ) => {
+			const expected = 3;
+			const actual = lists.length;
+			t.equal( actual, expected, 'API.getLists(): should return 3 lists.' );
+		});
+	API.getPrimaryList()
+		.then( ( list ) => {
+			const expected = 1;
+			const actual = list.id;
+			t.equal( actual, expected, 'API.getPrimaryList(): should return list id 1.' );
+		});
+	API.getDeletedLists()
+		.then( ( lists ) => {
+			const expected = 1;
+			const actual = lists.length;
+			t.equal( actual, expected, 'API.getLists(): should return 1 deleted list.' );
+		});
+	API.getListById( 2 )
+		.then( ( list ) => {
+			const expected = 'My Second List';
+			const actual = list.name;
+			t.equal( actual, expected, 'API.getListByID(2): should return list 2 named My Second List' );
+		});
+	API.getTodos( 2 )
+		.then( ( todos ) => {
+			const expected = 3;
+			const actual = todos.length;
+			t.equal( actual, expected, 'API.getTodos(2): should get todos from list 3. There should be 3 todos.' );
+		});
+	API.updateTodo( 3, 12, { title: 'List 3 - Todo 3, Updated' })
+		.then( ( todo ) => {
+			const expected = 'List 3 - Todo 3, Updated';
+			const expectedId = 12;
+			const actual = todo.title;
+			const actualId = todo.id;
+			t.equal( actual, expected, 'API.updateTodo(): should update title to List 3 - Todo 3, Updated' );
+			t.equal( actualId, expectedId, 'API.updateTodo(): should update todo with id of 12.' );
+		});
+	API.deleteTodo( 2, 7 )
+		.then( ( todos ) => {
+			const expectedNumberOfTodos = 1;
+			const actualNumberOfTodos = todos.length;
+			t.equal( actualNumberOfTodos, expectedNumberOfTodos, 'API.deleteTodo(): should return 1 todos.' );
+		});
+	API.completeTodo( 3, 14 )
+		.then( ( todo ) => {
+			const expectedTodoId = 14;
+			const expectedCompleted = true;
+			const actualTodosId = todo.id;
+			const actualCompleted = todo.completed;
+			t.equal( actualTodosId, expectedTodoId, 'API.completeTodo(): should update todo with id 14.' );
+			t.equal( actualCompleted, expectedCompleted, 'API.completeTodo(): should be completed' );
+		});
+	API.deleteList( 2 )
+		.then( ( list ) => {
+			const expected = 'My Second List';
+			const actual = list.name;
+			t.equal( actual, expected, 'API.deleteList(2): should delete list with name of My Second List' );
+		});
 	t.end();
 });
