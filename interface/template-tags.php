174d24176bb454a3aa81b8e4211859c422d24686
Eric Fuller <info@glassmoonmedia.com>
2017-03-24 16:32:31 -0400

    Add in some more static markup that helps make up the UI. Some of this will eventually be moved out into JS templates.

--- interface/template-tags.php
+++ interface/template-tags.php
@@ -17,8 +17,32 @@
  * @since  1.0.0
  */
 function wpt_container() {
+	// @todo Will want to move this markup out into JS template partials.
 	?>
-	<div id="wp-todo" class="<?php wpt_admin_class(); ?>"></div>
+	<div id="wp-todo" class="<?php wpt_admin_class(); ?>">
+		<section id="wp-todo-container" class="wp-todo-container">
+			<header id="wp-todo-header" class="wp-todo-header">
+				<h3>WP Todo</h3>
+			</header>
+			<nav id="wp-todo-navbar" class="wp-todo-navbar">
+				<ul class="wp-todo-menu">
+					<li class="wp-todo-menu-item"><a href="#">New</a></li>
+					<li class="wp-todo-menu-item"><a href="#">Lists</a></li>
+					<li class="wp-todo-menu-item"><a href="#">Configure</a></li>
+				</ul>
+			</nav>
+			<div id="wp-todo-content-container" class="wp-todo-content">
+				<div class="wp-todo-loader-container">
+					<div class="wp-todo-loader"></div>
+				</div>
+				<div id="wp-todo-list-container"></div>
+				<div id="new-list" class="panel-hidden"><h3>add a new list</h3></div>
+				<div id="lists" class="panel-hidden"><h3>Choose another list.</h3></div>
+				<div id="configure" class="panel-hidden"><h3>Configuration options</h3></div>
+			</div>
+			<footer id="wp-todo-footer" class="wp-todo-footer"><h5>Footer</h5></footer>
+		</section>
+	</div>
 	<?php
 }
 
