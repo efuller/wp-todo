4214ffeb177d64b9759327f3bfb53a18cf15bd59
Eric Fuller <info@glassmoonmedia.com>
2017-06-12 11:04:20 -0400

    Use a function with JSONServer to seed our API. This allows us to run uuid() and create unique ID's for todos.

---
+++ src/api/seed.js
@@ -0,0 +1,55 @@
+const uuid = require( 'uuid/v4' );
+
+module.exports = () => {
+	return {
+		config: {
+			'primary': 1,
+			'showCompleted': false,
+			'showDeleted': false
+		},
+		'todoLists': [
+			{
+				'id': 1,
+				'name': 'default list'
+			},
+			{
+				'id': 2,
+				'name': 'Another list'
+			}
+		],
+		'todos': [
+			{
+				'id': uuid(),
+				'title': 'Todo Number 1',
+				'description': 'Here is the description for number 1',
+				'todoListId': 1,
+				'completed': false,
+				'deleted': false
+			},
+			{
+				'id': uuid(),
+				'title': 'Todo Number 6',
+				'description': 'Here is the description for number 1',
+				'todoListId': 1,
+				'completed': true,
+				'deleted': false
+			},
+			{
+				'id': uuid(),
+				'title': 'Todo Number 2',
+				'description': 'Here is the description for number 2',
+				'todoListId': 1,
+				'completed': false,
+				'deleted': false
+			},
+			{
+				'id': uuid(),
+				'title': 'Todo Number 1',
+				'description': 'From list 2',
+				'todoListId': 2,
+				'completed': false,
+				'deleted': false
+			}
+		]
+	};
+};
