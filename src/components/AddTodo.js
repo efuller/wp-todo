abfbca0edadcbd05621d505dc22b011008069477
Eric Fuller <info@glassmoonmedia.com>
2017-08-14 15:12:32 -0400

    Initial class for AddTodo.

---
+++ src/components/AddTodo.js
@@ -0,0 +1,61 @@
+import API from '../api/API';
+import { events } from '../utilities/Events';
+import { appState } from '../utilities/State';
+import uuid from 'uuid/v4';
+import addToDoTemplate from '../views/addToDo.html';
+
+import $ from 'jQuery';
+
+class AddTodo {
+	constructor() {
+		this.cache();
+		this.render();
+		this.bindEvents();
+	}
+
+	render() {
+		this.$footer.html( addToDoTemplate() );
+	}
+
+	cache() {
+		this.$footer = $( '#wp-todo-footer' );
+	}
+
+	updateAddTodoState( todo ) {
+		const state = appState.getState();
+		const { todos } = state;
+
+		const newTodos = [
+			...todos,
+			todo
+		];
+
+		appState.setState({todos: newTodos});
+	}
+
+	bindEvents() {
+		const $form = $( '#add-todo-form' );
+		$form.submit( ( e ) => {
+			e.preventDefault();
+			const todo = $form.find( '#add-todo' ).val().trim();
+			const list = appState.getState().activeList || appState.getState().primaryList;
+
+			const newTodo = {
+				'id': uuid(),
+				'title': todo,
+				'description': 'Just something default',
+				'todoListId': list,
+				'completed': false,
+				'deleted': false
+			};
+
+			API.addTodo( list, newTodo )
+				.then( ( response ) => {
+					this.updateAddTodoState( response.data );
+					events.emit( 'add-todo', todo );
+				});
+		});
+	}
+}
+
+export default AddTodo;
